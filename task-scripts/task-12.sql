e3f0ac82051a665bb2d0068400c2f745ae21af3d
Stepan_Klimashin <stepan_klimashin@rntgroup.com>
2022-09-25 22:05:13 +0400

    Optimize query for task 12

--- task-scripts/task-12.sql
+++ task-scripts/task-12.sql
@@ -1,17 +1,11 @@
 -- 12. Select the biggest mark for each student and add text description for the mark (use COALESCE and WHEN operators)
 SELECT s.id AS student_id,
-	   MAX(er.mark) AS max_mark,
-	   CASE coalesce(MAX(er.mark), 0)
-		   WHEN 1 THEN 'BAD'
-		   WHEN 2 THEN 'BAD'
-		   WHEN 3 THEN 'BAD'
-		   WHEN 4 THEN 'AVERAGE'
-		   WHEN 5 THEN 'AVERAGE'
-		   WHEN 6 THEN 'AVERAGE'
-		   WHEN 7 THEN 'GOOD'
-		   WHEN 8 THEN 'GOOD'
-		   WHEN 9 THEN 'EXCELLENT'
-		   WHEN 10 THEN 'EXCELLENT'
+	   coalesce(MAX(er.mark), 0) AS max_mark,
+	   CASE
+		   WHEN MAX(er.mark) BETWEEN 1 AND 3 THEN 'BAD'
+		   WHEN MAX(er.mark) BETWEEN 4 AND 6 THEN 'AVERAGE'
+		   WHEN MAX(er.mark) BETWEEN 7 AND 8 THEN 'GOOD'
+		   WHEN MAX(er.mark) BETWEEN 9 AND 10 THEN 'EXCELLENT'
 		   ELSE 'not passed'
 	   END AS word_mark
 FROM exam_results er
